6597a79477743a124725aff9ec1bda77d2c227a8
douglasmatosdev <douglasmatosdev@gmail.com>
2023-04-19 19:57:28 -0300

    exercices

--- fifth_semester/algorithms_and_complexity/Functions/src/main/java/com/douglasmatosdev/Main.java
+++ fifth_semester/algorithms_and_complexity/Functions/src/main/java/com/douglasmatosdev/Main.java
@@ -1,10 +1,11 @@
 package com.douglasmatosdev;
 
 import com.douglasmatosdev.exercices.Exercice1;
+import com.douglasmatosdev.exercices.Exercice2;
 
 public class Main {
     public static void main(String[] args) {
         Exercice1.run();
+        Exercice2.run();
     }
-
 }
--- fifth_semester/algorithms_and_complexity/Functions/src/main/java/com/douglasmatosdev/exercices/Exercice2.java
+++ fifth_semester/algorithms_and_complexity/Functions/src/main/java/com/douglasmatosdev/exercices/Exercice2.java
@@ -1,2 +1,26 @@
-package com.douglasmatosdev.exercices;public class Exercice2 {
+package com.douglasmatosdev.exercices;
+
+public abstract class Exercice2 {
+    private static final int[] list = new int[]{0, 1, 2, 3, 4, 5};
+    private static final int n = list.length;
+
+    public static void run() {
+        System.out.println("Exercise 2");
+        f(n);
+        g(n);
+        System.out.println();
+    }
+
+    private static void f(int n) {
+        // f(n) = n + 4
+        System.out.println("f(" + n + ") = " + n + " + 4");
+        System.out.println("f(" + n + ") = " + (n + 4));
+    }
+
+    private static void g(int n) {
+        // g(n) = n²
+        System.out.println("g(" + n + ") = " + n + "²");
+        System.out.println("g(" + n + ") = " + (n * n));
+    }
+
 }
