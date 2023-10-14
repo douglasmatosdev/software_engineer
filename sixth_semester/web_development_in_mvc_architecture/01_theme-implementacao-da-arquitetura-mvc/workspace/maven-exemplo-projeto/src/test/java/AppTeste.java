be74899c21996cdae6486c9d4f08638a01f2932b
douglasmatosdev <douglasmatosdev@gmail.com>
2023-10-14 11:03:22 -0300

    modulo 1 - maven

---
+++ sixth_semester/web_development_in_mvc_architecture/01_theme-implementacao-da-arquitetura-mvc/workspace/maven-exemplo-projeto/src/test/java/AppTeste.java
@@ -0,0 +1,8 @@
+import org.junit.Assert;
+import org.junit.Test;
+public class AppTeste {
+    @Test
+    public void test() {
+        Assert.assertEquals("Este é o segundo exemplo!", new String("Este é o segundo exemplo!"));
+    }
+}
--- sixth_semester/web_development_in_mvc_architecture/01_theme-implementacao-da-arquitetura-mvc/worspace/estudos/src/main/java/com/example/estudos/repositories/LivroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.estudos.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.estudos.entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-	Livro findById(long id);	
-}
