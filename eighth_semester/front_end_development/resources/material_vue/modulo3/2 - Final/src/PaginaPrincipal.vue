22bafb2af9e2c7a2499a97de367d8e2f61bf097a
douglasmatosdev <douglasmatosdev@gmail.com>
2024-09-25 19:00:16 -0300

    material Vue

---
+++ eighth_semester/front_end_development/resources/material_vue/modulo3/2 - Final/src/PaginaPrincipal.vue
@@ -0,0 +1,33 @@
+
+<template>
+  <Cabecalho>
+
+    <template v-slot:subtitulo>
+        <h2>Subtítulo aqui</h2>
+    </template>
+
+    <template v-slot:extra>
+        <h3>Texto extra</h3>
+    </template>
+    
+  </Cabecalho>
+
+  <ListaCidades />
+</template>
+
+
+<script>
+import "./assets/estilo.css";
+import Cabecalho from "./components/Cabecalho.vue";
+import ListaCidades from "./components/ListaCidades.vue";
+
+export default {
+  components: { Cabecalho, ListaCidades },
+
+  methods: {},
+  computed: {},
+};
+</script>
+
+<style>
+</style>
