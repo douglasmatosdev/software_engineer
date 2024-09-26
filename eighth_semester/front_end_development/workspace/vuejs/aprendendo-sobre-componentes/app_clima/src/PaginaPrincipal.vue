d3a48057ec5aad80d6b53e043c1e03491fe36e2f
douglasmatosdev <douglasmatosdev@gmail.com>
2024-09-26 12:26:27 -0300

    Vue.js projeto de clima - componentes

---
+++ eighth_semester/front_end_development/workspace/vuejs/aprendendo-sobre-componentes/app_clima/src/PaginaPrincipal.vue
@@ -0,0 +1,23 @@
+<template>
+  <Cabecalho>
+    <template v-slot:subtitulo>
+      <h2>Subtitulo aqui</h2>
+    </template>
+    <template v-slot:extra>
+      <h3>Texto extra</h3>
+    </template>
+  </Cabecalho>
+  <ListCidades />
+</template>
+
+<script>
+import './assets/estilo.css'
+import Cabecalho from './components/Cabecalho.vue';
+import ListCidades from './components/ListCidades.vue';
+
+export default {
+  components: { Cabecalho, ListCidades },
+}
+</script>
+
+<style scoped></style>
